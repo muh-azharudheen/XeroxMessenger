e02fd1828834e9a12b7f170a0fe99b59d27b2a08
Mac <mac@Macs-MacBook-Pro.local>
2018-08-14 00:05:40 +0400

    implementing firebase photo upload

--- app/src/main/java/xerox/kotlinfirebasemessenger/RegisterActivity.kt
+++ app/src/main/java/xerox/kotlinfirebasemessenger/RegisterActivity.kt
@@ -1,14 +1,23 @@
 package xerox.kotlinfirebasemessenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
+
+    var selectedPhotoUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +34,32 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        selectphoto_button_register.setOnClickListener {
+            Log.d("Register Activity", "Try to show photo selector")
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            //proceed and check if the selected image was
+            Log.d("Register Activity", "Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,selectedPhotoUri)
+
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
+
+        }
+    }
+
 
     private fun performRegister(){
         val email = email_edittext_registration.text.toString()
@@ -43,10 +77,24 @@ class MainActivity : AppCompatActivity() {
                 .addOnCompleteListener {
                     if (!it.isSuccessful)return@addOnCompleteListener
                     Log.d("Main","Successfully created user with uuid : ${it.result.user.uid}")
+                    uploadImageToFirebase()
                 }
                 .addOnFailureListener {
                     Log.d("Main", "Failed to create user: ${it.message}")
                 }
          }
+
+    private fun uploadImageToFirebase(){
+        if (selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+                .addOnSuccessListener {
+
+                }
+
+
+    }
 
 }
