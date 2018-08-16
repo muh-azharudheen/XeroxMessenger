efc7b8e3bdce70287cd9593604d31ab58c02f32c
MyScrap <myscrap@apples-iMac.local>
2018-08-16 18:04:57 +0400

    added nav menu

--- app/src/main/java/xerox/kotlinfirebasemessenger/LatestMessageActivity.kt
+++ app/src/main/java/xerox/kotlinfirebasemessenger/LatestMessageActivity.kt
@@ -3,6 +3,8 @@ package xerox.kotlinfirebasemessenger
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessageActivity : AppCompatActivity() {
@@ -17,9 +19,37 @@ class LatestMessageActivity : AppCompatActivity() {
     private fun verifyUserISLoggedIn(){
         val uid = FirebaseAuth.getInstance().uid
         if (uid == null){
-            val intent = Intent(this, RegisterActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            startActivity(intent)
+            performRegisterActivity()
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_nav_latest_messages,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+
+        when (item?.itemId) {
+            R.id.new_message_menu -> {
+
+            }
+            R.id.sign_out_menu -> {
+                FirebaseAuth.getInstance().signOut()
+                performRegisterActivity()
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun performRegisterActivity(){
+        val intent = Intent(this, RegisterActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+
 }
